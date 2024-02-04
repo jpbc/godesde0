56d51a0eade61219e547c395e80d2a54cb8ca011
jpbc <jpbc80@gmail.com>
2024-02-04 19:45:53 -0300

    Segundo Commit

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/jpbc/godesde0/variables"
+import (
+	"fmt"
+
+	"github.com/jpbc/godesde0/variables"
+)
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1234)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
