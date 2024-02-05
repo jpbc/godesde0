789ab37896ae87a577fd27ca83b6316567f7ae92
jpbc <jpbc80@gmail.com>
2024-02-04 22:29:37 -0300

    Tercer Commit trabajando con bufio

--- main.go
+++ main.go
@@ -1,33 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
 
-	"github.com/jpbc/godesde0/ejercicios"
-	"github.com/jpbc/godesde0/variables"
+	//"runtime"
+
+	"github.com/jpbc/godesde0/teclado"
+	//"github.com/jpbc/godesde0/variables"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto(1234)
-	fmt.Println(estado)
-	fmt.Println(texto)
+	/*
+		estado, texto := variables.ConviertoaTexto(1234)
+		fmt.Println(estado)
+		fmt.Println(texto)
 
-	if os := runtime.GOOS; os == "linux." || os == "OS X." {
-		fmt.Println("Esto no es Windows es", os)
-	} else {
-		fmt.Println("Esto es Windows")
+		if os := runtime.GOOS; os == "linux." || os == "OS X." {
+			fmt.Println("Esto no es Windows es", os)
+		} else {
+			fmt.Println("Esto es Windows")
 
-	}
-	switch os := runtime.GOOS; os {
-	case "linux":
-		fmt.Println("Esto es Linux")
-	case "darwin":
-		fmt.Println("Esto es Darwin")
-	default:
-		fmt.Printf("%s \n", os)
-	}
-	numero, cadena := ejercicios.DevuelveIntString("58")
-	fmt.Println(numero)
-	fmt.Println(cadena)
+		}
+		switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Println("Esto es Linux")
+		case "darwin":
+			fmt.Println("Esto es Darwin")
+		default:
+			fmt.Printf("%s \n", os)
+		}
+		numero, cadena := ejercicios.DevuelveIntString("58")
+		fmt.Println(numero)
+		fmt.Println(cadena)
+	*/
+	teclado.IngresoNumeros()
+
 }
